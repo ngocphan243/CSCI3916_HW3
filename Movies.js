a57f2d5363ce8275118b8c2ae7ee6d5d6f8912e2
ngocphan243 <98306356+ngocphan243@users.noreply.github.com>
2022-03-13 23:34:32 -0600

    Update Movies.js

--- Movies.js
+++ Movies.js
@@ -5,6 +5,7 @@ var bcrypt = require('bcrypt-nodejs');
 
 mongoose.Promise = global.Promise;
 
+
 //mongoose.connect(process.env.DB, { useNewUrlParser: true });
 try {
     mongoose.connect( process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
